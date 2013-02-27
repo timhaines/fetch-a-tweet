5e862a0964c8839df2d5fa88189256c2e52b44a6
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-26 23:45:37 -0800

    removed twitter tokens from git repo

--- config/initializers/twitter_token.rb
+++ config/initializers/twitter_token.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 Twitter.configure do |config|
-  config.consumer_key = "wpGytCOMwahcu2NsIeDYQ"
-  config.consumer_secret = "qak5OnYJcYfTDQoPx0WXLuuwDIkGHE5wrj26uilA"
-  config.oauth_token = "14657786-vU7qs3chN6V6DCth82VzgZUJfpEvqLWiVoGzyEKFW"
-  config.oauth_token_secret = "9XlsFgf2Q3Gak2GJDNQRlvLylGmrZhWaKn9GWJ9n1s"
+  config.consumer_key = ""
+  config.consumer_secret = ""
+  config.oauth_token = ""
+  config.oauth_token_secret = ""
 end
